354a98a9402f42ba0107c8bf4a99619275641305
Hamza Alam <smhamza124@gmail.com>
2020-05-04 05:53:42 +0500

    Updates

---
+++ precache-manifest.16d0fb73a9b37cb627b245f661b23516.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c9f61e5c3d160b0709c8ed3e7250ac8",
+    "url": "/react-quiz-app/index.html"
+  },
+  {
+    "revision": "cc61459dc29d9bd47f04",
+    "url": "/react-quiz-app/static/css/main.502461e5.chunk.css"
+  },
+  {
+    "revision": "d1d8fa8c0511ce5458cf",
+    "url": "/react-quiz-app/static/js/2.234be9d3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-quiz-app/static/js/2.234be9d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc61459dc29d9bd47f04",
+    "url": "/react-quiz-app/static/js/main.2d3d820a.chunk.js"
+  },
+  {
+    "revision": "3b9154cd96ba08565b88",
+    "url": "/react-quiz-app/static/js/runtime-main.8da5d460.js"
+  }
+]);
